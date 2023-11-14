98f2ebf015e0accbd660a082ac9903f327c75888
Willy Arrojas <willyarrojas@gmail.com>
2023-11-14 13:02:39 +0100

    add topics until 192

--- src/app/pages/perfil/profile.component.ts
+++ src/app/pages/perfil/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { FileUploadService, TypeTable } from 'src/app/services/file-upload.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -11,21 +13,42 @@ import { UserService } from 'src/app/services/user.service';
 export class ProfileComponent implements OnInit {
 
   public profileForm: FormGroup;
+  public user: User;
+  public imageToUpload: File;
 
   constructor(
-    private userService: UserService
-  ) { }
+    private userService: UserService,
+    private fileUploadService: FileUploadService
+  ) {
+    this.user = userService.user;
+  }
+
   ngOnInit(): void {
     this.profileForm = new FormGroup({
-      name: new FormControl('User-X', Validators.required),
-      email: new FormControl('user.X@user.com', [Validators.required, Validators.email]),
+      name: new FormControl(this.user.name, Validators.required),
+      email: new FormControl(this.user.email, [Validators.required, Validators.email]),
     });
   }
+
+  uploadImage() {
+    this.fileUploadService
+      .updatePhoto(this.imageToUpload, TypeTable.USERS, this.user.uid)
+      .then(img => console.log(img));
+  }
+
+  changeImage(file: File) {
+    this.imageToUpload = file;
+  }
+
   updateProfile() {
-    console.log(this.profileForm.value);
     this.userService.updateProfile(this.profileForm.value)
       .subscribe({
-        next: (resp) => console.log(resp),
+        next: (resp) => {
+          const { name, email } = this.profileForm.value;
+          // for reference, all data is update
+          this.user.name = name;
+          this.user.email = email;
+        },
         error: (err) => console.log(err)
       });
   }
---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { UserService } from './user.service';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  constructor(
+    private userService: UserService
+  ) { }
+
+  async updatePhoto(
+    file: File,
+    type: TypeTable,
+    id: string) {
+
+    try {
+      const url = `${base_url}/uploads/${type}/${id}`;
+      const formData = new FormData();
+      formData.append('image', file);
+      const resp = await fetch(
+        url,
+        {
+          method: 'PUT',
+          headers: {
+            'x-token': this.userService.token
+          },
+          body: formData
+        },
+      );
+
+      const data = await resp.json();
+      return true;
+
+    } catch (error) {
+      console.log('updatePhoto > error: ', error);
+      return false;
+    }
+  }
+}
+
+export enum TypeTable {
+  USERS = 'users',
+  DOCTORS = 'doctors',
+  HOSPITALS = 'hospitals'
+}
